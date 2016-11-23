843f84a8580a38c6e182f2e7da345da01aae6709
odessabear@gmail.com <odessabear@gmail.com>
2016-11-23 14:04:24 +0200

    Added myfiles

---
+++ src/com/company/Task6.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by alexander.mikhailov on 23.11.2016.
+ */
+public class Task6 {
+    public static void main(String[] args){
+
+        for (int i = 1;i <= 99;i++){
+            if (i%2==1){
+                System.out.print( i + " , ");
+            }
+
+        }
+    }
+}
